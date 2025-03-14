3aaaa27dbb25a866c726c210ff51d93aa3042420
Alex Prasov <freezay00828002@gmail.com>
2025-03-15 00:23:19 +0200

    Working on exercise 3

--- Source.cpp
+++ Source.cpp
@@ -15,6 +15,7 @@
 #include <locale>
 #include <sstream>
 #include <regex>
+#include <unordered_map>
 
 
 using namespace std;
@@ -65,6 +66,13 @@ void putCertainWordsToAnotherFile(string& str, string& language, string& fileToP
 #pragma	endregion
 
 #pragma	region For Ex 3
+
+
+string transliterateFromUkrToEn(const string& text);
+
+string reverseTransliterateFromEnToUkr(const string& text);
+
+
 #pragma	endregion
 
 #pragma endregion
@@ -75,7 +83,7 @@ int main()
 
 	system("chcp 1251>null");
 
-//#pragma region Exercise 1
+#pragma region Exercise 1
 //
 //
 //	/*
@@ -196,7 +204,7 @@ int main()
 //	} while (choice != 4);
 // 
 //
-//#pragma endregion 
+#pragma endregion 
 
 
 #pragma region Exercise 2
@@ -208,89 +216,89 @@ int main()
 		Список неприйнятних слів знаходиться в іншому файлі.
 	*/
 
-	string  FileNameExercise2 = "Exercise2-text-to-modify.txt";
+	//string  FileNameExercise2 = "Exercise2-text-to-modify.txt";
 
-	string bannedWordsFile = "Exercise2-banned-words.txt";
+	//string bannedWordsFile = "Exercise2-banned-words.txt";
 
-	// ----------------------------------------           File 2 reset        -------------------------------------------
+	//// ----------------------------------------           File 2 reset        -------------------------------------------
 
-	vector<string> textForFileExerciseTwo = {
-		"This is a simple sample text file. It contains some words that might be considered inappropriate or unwanted.",
-		"Some people use bad words in their writing, but this file should be cleaned from all such words.",
-		"Let's make this file clean and friendly."
-	};
+	//vector<string> textForFileExerciseTwo = {
+	//	"This is a simple sample text file. It contains some words that might be considered inappropriate or unwanted.",
+	//	"Some people use bad words in their writing, but this file should be cleaned from all such words.",
+	//	"Let's make this file clean and friendly."
+	//};
 
-	resetFile(FileNameExercise2);
-	resetFile(bannedWordsFile);
-	
-	ofstream fileToAdd2(FileNameExercise2, ios::app);
-	if (!fileToAdd2)
-	{
-		cout << "Файл не знайдено." << endl;
-		return 1;
-	}
+	//resetFile(FileNameExercise2);
+	//resetFile(bannedWordsFile);
+	//
+	//ofstream fileToAdd2(FileNameExercise2, ios::app);
+	//if (!fileToAdd2)
+	//{
+	//	cout << "Файл не знайдено." << endl;
+	//	return 1;
+	//}
 
-	for (const string& newLineToAdd: textForFileExerciseTwo)
-	{
-		fileToAdd2 << newLineToAdd << "\n";
-	}
-	fileToAdd2.close();
+	//for (const string& newLineToAdd: textForFileExerciseTwo)
+	//{
+	//	fileToAdd2 << newLineToAdd << "\n";
+	//}
+	//fileToAdd2.close();
 
-	// -------------------------------------------------------------------------------------------------------------------
+	//// -------------------------------------------------------------------------------------------------------------------
 
-	vector<string>userWordsToPutIntoAnotherFile;
-	vector<pair<int, char>> positions;
+	//vector<string>userWordsToPutIntoAnotherFile;
+	//vector<pair<int, char>> positions;
 
-	int numberOfWords;
+	//int numberOfWords;
 
-	cout << endl << endl << "\033[45m Введення кількості слів \033[0m" << endl << endl;
+	//cout << endl << endl << "\033[45m Введення кількості слів \033[0m" << endl << endl;
 
-	while (true) {
-		cout << "Кількість слів для вводу: ";
-		cin >> numberOfWords;
+	//while (true) {
+	//	cout << "Кількість слів для вводу: ";
+	//	cin >> numberOfWords;
 
-		if (cin.fail()) {
-			cout << "\033[031m Помилка! Будь ласка, спробуйте ще раз.\033[0m\n";
-			cin.clear();
-			cin.ignore(1000, '\n');
-			continue;
-		}
-		if (numberOfWords < 1)
-		{
-			cout << "\033[031m Помилка! Будь ласка, Введіть кількість слов більше за 1.\033[0m\n";
-			continue;
-		}
+	//	if (cin.fail()) {
+	//		cout << "\033[031m Помилка! Будь ласка, спробуйте ще раз.\033[0m\n";
+	//		cin.clear();
+	//		cin.ignore(1000, '\n');
+	//		continue;
+	//	}
+	//	if (numberOfWords < 1)
+	//	{
+	//		cout << "\033[031m Помилка! Будь ласка, Введіть кількість слов більше за 1.\033[0m\n";
+	//		continue;
+	//	}
 
-		break;
-	}
+	//	break;
+	//}
 
-	cout << endl << endl << "\033[45m Введення слів \033[0m" << endl << endl;
-	for (size_t i = 0; i < numberOfWords; i++)
-	{
-		string word;
-		cout << (i + 1) << ". ";
-		cin >> word;
-		userWordsToPutIntoAnotherFile.push_back(word);
-	}
+	//cout << endl << endl << "\033[45m Введення слів \033[0m" << endl << endl;
+	//for (size_t i = 0; i < numberOfWords; i++)
+	//{
+	//	string word;
+	//	cout << (i + 1) << ". ";
+	//	cin >> word;
+	//	userWordsToPutIntoAnotherFile.push_back(word);
+	//}
 
-	
+	//
 
-	ifstream file2(FileNameExercise2, ios::in);
-	if (!file2)
-	{
-		cout << "Файл не знайдено." << endl;
-		return 1;
-	}
+	//ifstream file2(FileNameExercise2, ios::in);
+	//if (!file2)
+	//{
+	//	cout << "Файл не знайдено." << endl;
+	//	return 1;
+	//}
 
-	string current_line;
-	string languageMode = "en"; // Assume English for now
+	//string current_line;
+	//string languageMode = "en"; // Assume English for now
 
-	// Process each line of the file
-	while (getline(file2, current_line)) {
-		putCertainWordsToAnotherFile(current_line, languageMode, bannedWordsFile, userWordsToPutIntoAnotherFile);
-	}
+	//// Process each line of the file
+	//while (getline(file2, current_line)) {
+	//	putCertainWordsToAnotherFile(current_line, languageMode, bannedWordsFile, userWordsToPutIntoAnotherFile);
+	//}
 
-	file2.close();
+	//file2.close();
 
 #pragma endregion 
 
@@ -302,6 +310,91 @@ int main()
 	№3
 		Написати програму транслітерації з української на англійську. Дані для транслітерації беруться з файлу і записуються в інший файл.
 	*/
+
+string ukranianDataFile = "Exercise3-ukranian-data.txt";
+
+string englishDataFile = "Exercise3-english-data.txt";
+
+string fileToPushDataIn = "Exercise3-push-data.txt";
+
+ifstream file1(ukranianDataFile, ios::in);
+ifstream file2(englishDataFile, ios::in);
+ofstream file3(fileToPushDataIn, ios::out);
+
+if (!file1 || !file2 || !file3) {
+	cerr << "Помилка відкриття файлу!" << endl;
+	return 1;
+}
+
+string current_line1, current_line2;
+int userChoice;
+
+while (true) {
+	cout << endl;
+	cout << "\033[033mМеню:\n";
+	cout << "1. З української на англійську \n";
+	cout << "2. З англійської на україніську\n";
+	cout << "3. Вихід\033[0m\n";
+	cout << endl;
+	while (true)
+	{
+		cout << endl;
+		cout << "Введіть ваш вибір: ";
+		cin >> userChoice;
+
+		if (cin.fail()) {
+			cout << "\033[031m Помилка! Будь ласка, введіть число.\033[0m\n";
+			cin.clear(); 
+			cin.ignore(1000, '\n'); 
+			continue;
+		}
+
+		if (userChoice != 1 && userChoice != 2 && userChoice != 3)
+		{
+			cout << "\033[031m Помилка! Будь ласка, введіть число 1, 2 або 3.\033[0m\n";
+			continue;
+		}
+
+		break;
+
+	}
+
+
+	switch (userChoice)
+	{
+	case 1: 
+			cout << endl;
+			while (getline(file1, current_line1)) {
+				file3 << transliterateFromUkrToEn(current_line1) << endl;
+			}
+			cout << endl;
+			cout << "Транслітерація з української мови на англійську завершена!\nРезультат взятий із файлу \033[035m" << ukranianDataFile << "\033[0m збережений у файл з ім'ям \033[038m" << fileToPushDataIn << "\033[0m" << endl;
+			cout << endl;
+			break;
+		
+	case 2:
+			cout << endl;
+			while (getline(file2, current_line2)) {
+				file3 << reverseTransliterateFromEnToUkr(current_line2) << endl;
+			}
+			cout << endl;
+			cout << "Транслітерація з англійської мови на українську завершена!\nРезультат взятий із файлу \033[035m" << englishDataFile << "\033[0m збережений у файл з ім'ям \033[038m" << fileToPushDataIn << "\033[0m" << endl;
+			cout << endl;
+			break;
+		
+	case 3:
+			cout << endl;
+			file1.close();
+			file2.close();
+			file3.close();
+			cout << "Вихід...\n";
+			break;
+	default:
+		break;
+	}
+
+
+}
 
 
 #pragma endregion 
@@ -581,6 +674,69 @@ void putCertainWordsToAnotherFile(string& str, string& language, string& fileToP
 
 
 #pragma	region Functions for Ex 3
+
+string transliterateFromUkrToEn(const string& text) {
+	unordered_map<char, string> translit = {
+		{'А', "A"}, {'Б', "B"}, {'В', "V"}, {'Г', "H"}, {'Ґ', "G"}, {'Д', "D"}, {'Е', "E"}, {'Є', "Ye"},
+		{'Ж', "Zh"}, {'З', "Z"}, {'И', "Y"}, {'І', "I"}, {'Ї', "Yi"}, {'Й', "Y"}, {'К', "K"}, {'Л', "L"},
+		{'М', "M"}, {'Н', "N"}, {'О', "O"}, {'П', "P"}, {'Р', "R"}, {'С', "S"}, {'Т', "T"}, {'У', "U"},
+		{'Ф', "F"}, {'Х', "Kh"}, {'Ц', "Ts"}, {'Ч', "Ch"}, {'Ш', "Sh"}, {'Щ', "Shch"}, {'Ь', ""}, {'Ю', "Yu"}, {'Я', "Ya"},
+		{'а', "a"}, {'б', "b"}, {'в', "v"}, {'г', "h"}, {'ґ', "g"}, {'д', "d"}, {'е', "e"}, {'є', "ie"},
+		{'ж', "zh"}, {'з', "z"}, {'и', "y"}, {'і', "i"}, {'ї', "i"}, {'й', "i"}, {'к', "k"}, {'л', "l"},
+		{'м', "m"}, {'н', "n"}, {'о', "o"}, {'п', "p"}, {'р', "r"}, {'с', "s"}, {'т', "t"}, {'у', "u"},
+		{'ф', "f"}, {'х', "kh"}, {'ц', "ts"}, {'ч', "ch"}, {'ш', "sh"}, {'щ', "shch"}, {'ь', ""}, {'ю', "iu"}, {'я', "ia"}
+	};
+
+	string result;
+	for (char c : text) {
+		if (translit.count(c)) {
+			result += translit[c];
+		}
+		else {
+			result += c;
+		}
+	}
+	return result;
+}
+
+
+string reverseTransliterateFromEnToUkr(const string& text) {
+	unordered_map<string, char> reverseTranslit = {
+		{"A", 'А'}, {"B", 'Б'}, {"V", 'В'}, {"H", 'Г'}, {"G", 'Ґ'}, {"D", 'Д'}, {"E", 'Е'}, {"Ye", 'Є'},
+		{"Zh", 'Ж'}, {"Z", 'З'}, {"Y", 'И'}, {"I", 'І'}, {"Yi", 'Ї'}, {"K", 'К'}, {"L", 'Л'},
+		{"M", 'М'}, {"N", 'Н'}, {"O", 'О'}, {"P", 'П'}, {"R", 'Р'}, {"S", 'С'}, {"T", 'Т'}, {"U", 'У'},
+		{"F", 'Ф'}, {"Kh", 'Х'}, {"Ts", 'Ц'}, {"Ch", 'Ч'}, {"Sh", 'Ш'}, {"Shch", 'Щ'}, {"Yu", 'Ю'}, {"Ya", 'Я'},
+		{"a", 'а'}, {"b", 'б'}, {"v", 'в'}, {"h", 'г'}, {"g", 'ґ'}, {"d", 'д'}, {"e", 'е'}, {"ie", 'є'},
+		{"zh", 'ж'}, {"z", 'з'}, {"y", 'и'}, {"i", 'і'}, {"i", 'ї'}, {"k", 'к'}, {"l", 'л'},
+		{"m", 'м'}, {"n", 'н'}, {"o", 'о'}, {"p", 'п'}, {"r", 'р'}, {"s", 'с'}, {"t", 'т'}, {"u", 'у'},
+		{"f", 'ф'}, {"kh", 'х'}, {"ts", 'ц'}, {"ch", 'ч'}, {"sh", 'ш'}, {"shch", 'щ'}, {"iu", 'ю'}, {"ia", 'я'}
+	};
+
+	string result, temp;
+	for (size_t i = 0; i < text.size(); ++i) {
+		temp += text[i];
+
+		if (i + 1 < text.size()) {
+			temp += text[i + 1];
+		}
+
+		if (reverseTranslit.count(temp)) {
+			result += reverseTranslit[temp];
+			if (temp.size() == 2) i++;
+			temp.clear();
+		}
+		else if (reverseTranslit.count(string(1, text[i]))) {
+			result += reverseTranslit[string(1, text[i])];
+			temp.clear();
+		}
+		else if (temp.size() == 2) {
+			result += temp[0];
+			temp.erase(0, 1);
+		}
+	}
+	result += temp;
+	return result;
+}
 
 
 #pragma	endregion
